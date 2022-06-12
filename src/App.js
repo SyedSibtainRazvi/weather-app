bf8f93eeb07e46a600c24c55eb4c726084191636
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-06-12 20:19:41 +0530

    Style: Format document

--- src/App.js
+++ src/App.js
@@ -18,47 +18,47 @@ function App() {
   }
 
   return (
-      <div className="app">
-        <div className="search">
-          <p>WeatherU</p>
-          <input
-            value={location}
-            onChange={event => setLocation(event.target.value)}
-            onKeyPress={searchLocation}
-            placeholder='Enter Location'
-            type="text" />
+    <div className="app">
+      <div className="search">
+        <p>WeatherU</p>
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p> {data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p className='bold'>{data.weather[0].main}</p> : null}
+          </div>
         </div>
-        <div className="container">
-          <div className="top">
-            <div className="location">
-              <p> {data.name}</p>
+
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
+              <p>Minimum</p>
             </div>
-            <div className="temp">
-              {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
             </div>
-            <div className="description">
-              {data.weather ? <p className='bold'>{data.weather[0].main}</p> : null}
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
             </div>
           </div>
-
-          {data.name !== undefined &&
-            <div className="bottom">
-              <div className="feels">
-                {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
-                <p>Minimum</p>
-              </div>
-              <div className="humidity">
-                {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-                <p>Humidity</p>
-              </div>
-              <div className="wind">
-                {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-                <p>Wind Speed</p>
-              </div>
-            </div>
-          }
-        </div>
+        }
       </div>
+    </div>
   );
 }
 
